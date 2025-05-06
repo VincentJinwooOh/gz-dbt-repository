6af26693a25368c44fabcca9d42fb5c486c5ab99
jwzzang9696@naver.com <jwzzang9696@naver.com>
2025-05-06 20:18:28 +0000

    product id added

--- models/intermidiate/int_sales_margin.sql
+++ models/intermidiate/int_sales_margin.sql
@@ -1,4 +1,5 @@
-select sales.quantity * pro.purchase_price as purchase_cost
+select products_id
+,      sales.quantity * pro.purchase_price as purchase_cost
 ,      sales.revenue - sales.quantity * pro.purchase_price as margin
 from {{ ref("stg_raw__sales") }} as sales
 join {{ ref("stg_raw__product") }} as pro
