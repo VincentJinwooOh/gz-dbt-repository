0835097cbe097e9b8b0b7dbf9ce03654f76299ef
jwzzang9696@naver.com <jwzzang9696@naver.com>
2025-05-06 21:36:46 +0000

    sales fix

--- models/intermidiate/int_orders_operational.sql
+++ models/intermidiate/int_orders_operational.sql
@@ -1,18 +1,18 @@
-with orders_margin as (select sales.orders_id
-,      sales.date_date
-,      sum(sales.revenue) as total_revenue
-,      sum(sales.quantity) as total_quantity
-,      sum(sales.quantity * pro.purchase_price) as total_purchase_cost
-,      sum(sales.revenue - sales.quantity * pro.purchase_price) as total_margin
-from {{ ref("stg_raw__sales") }} as sales
-join {{ ref("stg_raw__product") }} as pro
-using(products_id)
-group by sales.orders_id
-,      sales.date_date)
+-- with orders_margin as (select sales.orders_id
+-- ,      sales.date_date
+-- ,      sum(sales.revenue) as total_revenue
+-- ,      sum(sales.quantity) as total_quantity
+-- ,      sum(sales.quantity * pro.purchase_price) as total_purchase_cost
+-- ,      sum(sales.revenue - sales.quantity * pro.purchase_price) as total_margin
+-- from {{ ref("stg_raw__sales") }} as sales
+-- join {{ ref("stg_raw__product") }} as pro
+-- using(products_id)
+-- group by sales.orders_id
+-- ,      sales.date_date)
 
 select *
 ,      total_margin+ship.shipping_fee-ship.logcost-ship.ship_cost operational_margin
-from orders_margin
+from {{ref('int_orders_margin')}}
 join {{ ref("stg_raw__ship")}} As ship
 using(orders_id)
 order by orders_id desc
--- models/intermidiate/int_sales_margin.sql
+++ models/intermidiate/int_sales_margin.sql
@@ -1,6 +1,6 @@
-select products_id
+select *
 ,      sales.quantity * pro.purchase_price as purchase_cost
 ,      sales.revenue - sales.quantity * pro.purchase_price as margin
 from {{ ref("stg_raw__sales") }} as sales
-join {{ ref("stg_raw__product") }} as pro
+left join {{ ref("stg_raw__product") }} as pro
 using(products_id)
