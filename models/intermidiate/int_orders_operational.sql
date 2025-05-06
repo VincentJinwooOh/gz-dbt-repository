4b7df7a9e9f83b5df9018c27d5a7556c6ed153d2
jwzzang9696@naver.com <jwzzang9696@naver.com>
2025-05-06 22:07:10 +0000

    operational fix

--- models/intermidiate/int_orders_operational.sql
+++ models/intermidiate/int_orders_operational.sql
@@ -1,18 +1,16 @@
--- with orders_margin as (select sales.orders_id
--- ,      sales.date_date
--- ,      sum(sales.revenue) as total_revenue
--- ,      sum(sales.quantity) as total_quantity
--- ,      sum(sales.quantity * pro.purchase_price) as total_purchase_cost
--- ,      sum(sales.revenue - sales.quantity * pro.purchase_price) as total_margin
--- from {{ ref("stg_raw__sales") }} as sales
--- join {{ ref("stg_raw__product") }} as pro
--- using(products_id)
--- group by sales.orders_id
--- ,      sales.date_date)
 
-select *
-,      total_margin+ship.shipping_fee-ship.logcost-ship.ship_cost operational_margin
+
+select orders_id
+,      date_date
+,      round(margin+ship.shipping_fee-ship.logcost-ship.ship_cost,2) as operational_margin
+,      revenue
+,      quantity
+,      purchase_cost
+,      margin
+,      ship.shipping_fee
+,      ship.logcost
+,      ship.ship_cost
 from {{ref('int_orders_margin')}}
-join {{ ref("stg_raw__ship")}} As ship
+left join {{ ref("stg_raw__ship")}} As ship
 using(orders_id)
-order by orders_id desc
+
