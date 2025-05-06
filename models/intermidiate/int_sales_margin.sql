e4798085e180077b76effb3084dbc14fabf113df
jwzzang9696@naver.com <jwzzang9696@naver.com>
2025-05-06 21:38:08 +0000

    sales fix again

--- models/intermidiate/int_sales_margin.sql
+++ models/intermidiate/int_sales_margin.sql
@@ -1,6 +1,6 @@
 select *
-,      sales.quantity * pro.purchase_price as purchase_cost
-,      sales.revenue - sales.quantity * pro.purchase_price as margin
+,      sales.quantity * pro.purchase_price as total_purchase_cost
+,      sales.revenue - sales.quantity * pro.purchase_price as total_margin
 from {{ ref("stg_raw__sales") }} as sales
 left join {{ ref("stg_raw__product") }} as pro
 using(products_id)
