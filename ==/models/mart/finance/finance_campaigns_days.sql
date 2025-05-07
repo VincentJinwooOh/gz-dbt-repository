34f676b2878c8f4e1db7967e01394877f69e9ff8
Jinwoo Oh <jwzzang9696@naver.com>
2025-05-07 20:29:00 +0000

    finance_days_campaigns

---
+++ ==/models/mart/finance/finance_campaigns_days.sql
@@ -0,0 +1,18 @@
+select date_date
+,      round(operational_margin - icd.ads_cost,0) as ads_margin
+,      avg_basket
+,      operational_margin
+,      icd.ads_cost as ads_cost
+,      icd.ads_impression as ads_impression
+,      icd.ads_clicks as ads_clicks
+,      quantity
+,      revenue
+,      purchase_cost
+,      margin
+,      shipping_fee
+,      logcost
+,      ship_cost
+from {{ref('finance_days')}} 
+left join {{ref('int_campaigns_day')}} as icd
+using (date_date)
+order by date_date desc
